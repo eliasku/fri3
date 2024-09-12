743cc307eac235cec3ef79e8b641bec5a153a6b4
eliasku <deadbabe@gmail.com>
2024-09-12 11:57:57 +0300

    13093 - fix wasm instantiation

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,7 @@
 import wasm from "../zig-out/bin/main.wasm?url";
 import { importMap, run } from "./lib";
 
-WebAssembly.instantiateStreaming(fetch(wasm), importMap).then((source) =>
-  run(source.instance),
-);
+fetch(wasm)
+  .then(v => v.arrayBuffer())
+  .then(i => WebAssembly.instantiate(i, importMap))
+  .then(s => run(s.instance));
